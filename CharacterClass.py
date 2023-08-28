3e9f6a8f5fb7bfb4f8bfee974ead3cc447e47a9a
Raul Sanchez Jr <96601235+rauls026@users.noreply.github.com>
2023-08-28 14:38:15 -0400

    Update CharacterClass.py
    This commit adds jumping capabilities to characters with the correct physics jump formula.

--- CharacterClass.py
+++ CharacterClass.py
@@ -6,37 +6,64 @@ environment = environmentals()
 #Creating a class for characters.
 class Character:
     def __init__(self, x, y, width, height, color):
-        self.rectangle = pygame.Rect(100, 100, 75, 75)
+        self.rectangle = pygame.Rect(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
         self.color = color
+        self.jumpProgressionCount = 10 #Jump speed for our jumping physics formula.
+        self.charIsJumping = False 
+        self.velocity = 5 #Player movement speed
     
     def update(self):
         #This function is for character movements, animations
         key = pygame.key.get_pressed()
-        velocity = 5
     
-        if key[pygame.K_UP]:
-            self.rectangle.y -= velocity 
+        if key[pygame.K_LEFT] and self.x > self.velocity:
+            self.x -= self.velocity
+
+        if key[pygame.K_RIGHT] and self.x < environment.getScreenWidth() - self.width - self.velocity:
+            self.x += self.velocity
+        
+        if not (self.charIsJumping): #IF the character is not jumping
+            if key[pygame.K_UP] and self.y > self.velocity:
+                self.y -= self.velocity 
+            
+            if key[pygame.K_DOWN] and self.y < environment.getScreenHeight() - self.height - self.velocity:
+                self.y += self.velocity
+
+            if key[pygame.K_SPACE]:
+                self.charIsJumping = True
     
-        if key[pygame.K_DOWN]:
-            self.rectangle.y += velocity
+        else: #Quadratic equation for jumping
+            if self.jumpProgressionCount >= -10:
+                neg = 1
+                if self.jumpProgressionCount < 0:
+                    neg = -1
+                self.y -= (self.jumpProgressionCount ** 2) * 0.5 * neg
+                self.jumpProgressionCount -= 1
+            else:
+                self.charIsJumping = False
+                self.jumpProgressionCount = 10
 
-        if key[pygame.K_LEFT]:
-            self.rectangle.x -= velocity
+        #Checking boundaries
+        
+        if self.x < 0:
+            self.x = 0
 
-        if key[pygame.K_RIGHT]:
-            self.rectangle.x += velocity
+        if self.x > environment.getScreenWidth() - self.width:
+            self.x = environment.getScreenWidth() - self.width
         
-        if self.rectangle.left < 0:
-            self.rectangle.left = 0
-
-        if self.rectangle.right > environment.getScreenWidth():
-            self.rectangle.right = environment.getScreenWidth()
+        if self.y < 0:
+            self.y = 0
         
-        if self.rectangle.top < 0:
-            self.rectangle.top = 0
+        if self.y > environment.getScreenHeight() - self.height:
+            self.y = environment.getScreenHeight() - self.height
         
-        if self.rectangle.bottom > environment.getScreenHeight():
-            self.rectangle.bottom = environment.getScreenHeight() 
+        #Updatung the position of the rectangle.
+        self.rectangle.x = self.x
+        self.rectangle.y = self.y
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rectangle)
